05ff37774412e6ee9893cb635c6b42e6cf4dcc3a
Matthias Rack <matthiasrack@yahoo.de>
2024-01-23 03:57:03 +0100

    cross and circle animation

--- tictactoc/script.js
+++ tictactoc/script.js
@@ -11,6 +11,7 @@ let fields = [
 ]
 
 function init() {
+    
     render();
 }
 
@@ -18,7 +19,7 @@ function render() {
     const contentDiv = document.getElementById('content');
     contentDiv.innerHTML = ''; // Clear previous content
 
-    const table = document.createElement('table');
+    let table = document.createElement('table');
 
     for (let i = 0; i < 3; i++) {
         const row = document.createElement('tr');
@@ -29,9 +30,9 @@ function render() {
             const fieldValue = fields[index];
 
             if (fieldValue === 'circle') {
-                cell.textContent = 'o';
+                cell.innerHTML = generateCircleSVG();
             } else if (fieldValue === 'cross') {
-                cell.textContent = 'x';
+                cell.innerHTML = generateCrossSVG();
             }
 
             row.appendChild(cell);
@@ -41,5 +42,41 @@ function render() {
     }
 
     contentDiv.appendChild(table);
+   
 }
 
+function generateCircleSVG() {
+    const color = '#00B0EF';
+    const width = 70;
+    const height = 70;
+
+    return `<svg width="${width}" height="${height}">
+              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.8s" fill="freeze" />
+              </circle>
+            </svg>`;
+}
+
+function generateCrossSVG() {
+    const color = '#FFC000';
+    const width = 70;
+    const height = 70;
+
+
+    const svgCode = `
+    <svg width="${width}" height="${height}">
+    <line x1="0" y1="0" x2="${width}" y2="${height}"
+      stroke="${color}" stroke-width="5">
+      <animate attributeName="x2" values="0; ${width}" dur="300ms" />
+      <animate attributeName="y2" values="0; ${height}" dur="300ms" />
+    </line>
+    <line x1="${width}" y1="0" x2="0" y2="${height}"
+      stroke="${color}" stroke-width="5">
+      <animate attributeName="x2" values="${width}; 0" dur="300ms" />
+      <animate attributeName="y2" values="0; ${height}" dur="300ms" />
+    </line>
+  </svg>
+`;
+
+    return svgCode;
+}
